1a1f4b0ec148dd8b0a40bbc3c208129150e73317
Nick <0xb000@gmail.com>
2020-01-19 23:54:17 +0200

    Add an example from wlroots distribution

--- cmake/settings.cmake
+++ cmake/settings.cmake
@@ -2,3 +2,5 @@
 set(CXX_STANDARD 17)
 #set_property(TARGET tgt PROPERTY CXX_STANDARD 17)
 
+add_definitions(-DWLR_USE_UNSTABLE -Wno-undef)
+
--- cmake/targets.cmake
+++ cmake/targets.cmake
@@ -1,7 +1,20 @@
 
+set(COMMON_LIBRARIES
+  ${WLROOTS_LIBS}
+  ${WAYLAND_LIBS}
+  GLESv2
+  xkbcommon
+)
+
+message("Link libs ${COMMON_LIBRARIES}")
+
 ####
 add_executable(rwc-init src/sandbox/hello-rwc.cc)
 target_include_directories(rwc-init PRIVATE ${WLROOTS_INCLUDE})
-target_link_libraries(rwc-init ${WLROOTS_LIBS})
+target_link_libraries(rwc-init ${COMMON_LIBRARIES})
 
+####
+add_executable(wlr-simple src/sandbox/simple.cc)
+target_include_directories(wlr-simple PRIVATE ${WLROOTS_INCLUDE})
+target_link_libraries(wlr-simple ${COMMON_LIBRARIES})
 
